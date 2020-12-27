97c2fed8c9cc0756305603b5200feaf52cf9dc1b
dbydd <1992003927@qq.com>
2020-12-27 19:11:38 +0800

    wait for algorithm

--- src/main/java/gkk/player_abilities/world_saved_data/AbilitiesData.java
+++ src/main/java/gkk/player_abilities/world_saved_data/AbilitiesData.java
@@ -2,11 +2,15 @@ package gkk.player_abilities.world_saved_data;
 
 import gkk.player_abilities.AbilityContainer;
 import gkk.player_abilities.Player_abilities;
+import net.minecraft.nbt.NBTBase;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.world.World;
 import net.minecraft.world.storage.WorldSavedData;
+import net.minecraftforge.common.util.Constants;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public class AbilitiesData extends WorldSavedData {
@@ -30,16 +34,54 @@ public class AbilitiesData extends WorldSavedData {
         return map.containsKey(playerID) && map.get(playerID).containsKey(name);
     }
 
-    //todo unFinished
-
     @Override
     public void readFromNBT(NBTTagCompound nbt) {
+        if (nbt.hasKey("list")) {
+            NBTTagList list = nbt.getTagList("list", Constants.NBT.TAG_LIST);
+            for (NBTBase nbtBase : list) {
+                if (nbtBase instanceof NBTTagCompound) {
+                    NBTTagCompound nbtTagCompound = (NBTTagCompound) nbtBase;
+                    UUID player = UUID.fromString(nbtTagCompound.getString("player"));
+                    NBTTagList abilities = nbtTagCompound.getTagList("abilities", Constants.NBT.TAG_LIST);
+                    HashMap<String, AbilityContainer> abilityContainerHashMap = new HashMap<>();
 
+                    for (NBTBase ability : abilities) {
+                        if (ability instanceof NBTTagCompound) {
+                            NBTTagCompound tagCompound = (NBTTagCompound) ability;
+                            String abilityName = tagCompound.getString("ability");
+                            abilityContainerHashMap.put(abilityName, AbilityContainer.deserializeNBT(tagCompound.getCompoundTag("data"), ));
+                        }
+                    }
+                    map.put(player, abilityContainerHashMap);
+                }
+            }
+        }
     }
 
     @Override
     public NBTTagCompound writeToNBT(NBTTagCompound compound) {
-        return null;
+        NBTTagList list = new NBTTagList();
+
+        for (Map.Entry<UUID, HashMap<String, AbilityContainer>> uuidHashMapEntry : map.entrySet()) {
+            HashMap<String, AbilityContainer> abilityContainerHashMap = uuidHashMapEntry.getValue();
+
+            NBTTagCompound nbtTagCompound = new NBTTagCompound();
+            nbtTagCompound.setString("player", uuidHashMapEntry.getKey().toString());
+
+            NBTTagList abilityList = new NBTTagList();
+            for (Map.Entry<String, AbilityContainer> stringAbilityContainerEntry : abilityContainerHashMap.entrySet()) {
+                NBTTagCompound abilityTag = new NBTTagCompound();
+                abilityTag.setString("ability", stringAbilityContainerEntry.getKey());
+                abilityTag.setTag("data", stringAbilityContainerEntry.getValue().serializeNBT());
+                abilityList.appendTag(abilityTag);
+            }
+
+            nbtTagCompound.setTag("abilities", abilityList);
+
+            list.appendTag(nbtTagCompound);
+        }
+        compound.setTag("list", compound);
+        return compound;
     }
 
 }
